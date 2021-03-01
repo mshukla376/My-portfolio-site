8397f068d5bc1a05dda612ac5de9ca53f4ba1873
mshukla376 <mshukla376@gmail.com>
2021-03-01 23:33:20 +0530

    changes in settings

--- pr/settings.py
+++ pr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+ek^2g2d9kb(nro84-t8hvfks4ohv1w&oqs3%i3j9gen&f0ila'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
